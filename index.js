dac9c6127eeb1522d2677c566d73e6cec58e3f43
mohammadashrafulislam <ixdell41@gmail.com>
2023-05-21 08:48:01 +0600

    delete toy from db

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
       const result = await toysCollection.find({sellerEmail: req.params.email}).toArray();
       res.send(result)
     })
+    // delete toy from db
+    app.delete('/toy/:id', async(req, res)=>{
+     const id = req.params.id;
+     const query = {_id: new ObjectId(id)}
+     const result = await toysCollection.deleteOne(query);
+     res.send(result)
+    })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
